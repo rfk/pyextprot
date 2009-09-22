f3e1801d934b950fffeacdd620bef7b6eb4b04c7
Ryan Kelly <ryan@rfk.id.au>
2009-09-22 19:35:56 +1000

    (split)add testcase for command-line compilation

--- extprot/tests/test_compiler.py
+++ extprot/tests/test_compiler.py
@@ -5,7 +5,11 @@ from os import path
 import tempfile
 import unittest
 
+import subprocess
+PIPE = subprocess.PIPE
+
 import extprot
+import extprot.compiler
 from extprot.types import *
 
 pfile = path.join(path.dirname(__file__),"../../../examples/address_book.proto")
@@ -51,4 +55,19 @@ class TestCompiler(unittest.TestCase):
         self.assertTrue("class optional(types.Union):" in out1)
         self.assertTrue("class Set(types.Option):" in out1)
 
+    def test_compile_module(self):
+        cfile = extprot.compiler.__file__        
+        cmd = [sys.executable,cfile]
+        if sys.platform == "win32":
+            sep = ";"
+        else:
+            sep = ":"
+        env = dict(PYTHONPATH=sep.join(sys.path))
+        infile = open(pfile)
+        p = subprocess.Popen(cmd,stdin=infile,stdout=PIPE,stderr=PIPE,env=env)
+        (stdout,stderr) = p.communicate()
+        self.assertEquals(stderr,"")
+        self.assertTrue("class person(types.Message):" in stdout)
+        self.assertTrue("class optional(types.Union):" in stdout)
+        self.assertTrue("class Set(types.Option):" in stdout)
 
